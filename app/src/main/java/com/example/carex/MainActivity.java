570cd8960ddd47b1e60e1b0735c054b680b8d9ff
IqrarH <iqrarhussain471@gmail.com>
2022-01-25 23:32:38 +0500

    My Items activity

--- app/src/main/java/com/example/carex/MainActivity.java
+++ app/src/main/java/com/example/carex/MainActivity.java
@@ -1,17 +1,67 @@
 package com.example.carex;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ActionMenuView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setNavigationViewListener();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_menu);
+        navigationView.setNavigationItemSelectedListener(this);
+        MenuItem item = navigationView.getMenu().findItem(R.id.nav_myItems);
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Log.d("TAD", "MENU");
+                return false;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Log.d("Tag", "Clicked");
+        Log.d("TAG", String.valueOf(item.getItemId()));
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setNavigationViewListener() {
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d("Tag", String.valueOf(item.getItemId()));
+        return true;
     }
 
     public void onDonateClick(View view) {
@@ -58,6 +108,11 @@ public class MainActivity extends AppCompatActivity {
     public void onClothesClick(View view) {
         Intent intent = new Intent(MainActivity.this, ExploreActivity.class);
         intent.putExtra("category", "Clothes");
+        startActivity(intent);
+    }
+
+    public void onMyItemsClick(View view) {
+        Intent intent = new Intent(MainActivity.this, MyItems.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/carex/myRecyclerViewAdapter2.java
@@ -0,0 +1,29 @@
+package com.example.carex;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class myRecyclerViewAdapter2 extends RecyclerView.Adapter<myRecyclerViewAdapter.MyViewHolder>{
+    List<ItemModel> itemsList;
+    private static Context context;
+    @NonNull
+    @Override
+    public myRecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myRecyclerViewAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
